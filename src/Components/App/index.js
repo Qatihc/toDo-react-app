4c525122335280f2d06991369a0f2bcdde4d373f
Matias Casali <matias_casali@hotmail.com>
2021-02-27 20:33:03 -0300

    Enviroment setup

---
+++ src/Components/App/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+import './stylee.scss'
+class App extends Component {
+  render() {
+    return <h1>reaasdasdaaact</h1>;
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./src/index.html'),
+    })
+  ]
+}
